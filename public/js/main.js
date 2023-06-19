4440d5c089dbeeb68c41101f9e82fdf49d848ad6
Alexis Heras <tetris99dj@gmail.com>
2023-06-18 20:17:23 -0600

    Mejorando intuicuón de usuario, y modificando UI/UX

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,12 @@
-function articles(params) {
-        $("#wel").hide();
-        $("#notesDiv").show();
-}
-function inicio(params) {
-    $("#notesDiv").hide();
-    $("#wel").show();
-}
-
-
-function random(min, max) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
-}
-
-function randomNumber() {
-    return Math.floor(Math.random() * 16) - 5;
-  }
-
 function light() {
-
     UIkit.notification({
         message: "Para esperar más rapido, solo requieres ir a 250,000 km/s",
         status: 'primary',
         pos: 'top-center',
         timeout: 5000
     });
-    
 }
 
-
-$('#notes').click(() => {
-    articles();  
-})
-
-$('#inicio').click(() => {
-    inicio();
-})
+function showArticle() {
+    
+}
