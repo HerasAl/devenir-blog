21a4f9e06466dddbd0b8eb98b88a20c68f18dc48
Alexis Heras <tetris99dj@gmail.com>
2023-07-20 19:17:49 -0600

    aliciaUX

--- public/sareh/alicIA/js/main.js
+++ public/sareh/alicIA/js/main.js
@@ -1,88 +1,35 @@
-if ('webkitSpeechRecognition' in window && 'speechSynthesis' in window) {
-    const recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.lang = 'es-ES';
-
-    
-
-    const synth = window.speechSynthesis;
-
-    var voices = synth.getVoices();
 
 
-    const startBtn = document.getElementById('start-btn');
-    const stopBtn = document.getElementById('stop-btn');
-    const transcription = document.getElementById('transcription');
-    const textInput = document.getElementById('text-input');
-    const speakBtn = document.getElementById('speak-btn');
+navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(function(stream) {
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
 
-    recognition.onresult = function(event) {
-        const result = event.results[event.resultIndex];
-        const transcript = result[0].transcript;
+    var source = audioContext.createMediaStreamSource(stream);
+    source.connect(analyser);
 
-        transcription.textContent = transcript;
+    var waveform = document.getElementById('waveform');
+    waveform.classList.add('uk-box-shadow-small');
 
-        // Detener la transcripción cuando se haya terminado de hablar
-        if (result.isFinal) {
-            recognition.stop();
+    function animateWaveform() {
+      requestAnimationFrame(animateWaveform);
 
-            // Repetir lo que se dijo usando la síntesis de voz
-            speakText(transcript, enableRecognition);
-              
-    // Recorrer las voces y mostrar la información
-    voices.forEach(function(voice, index) {
-        console.log('Voz ' + (index + 1) + ':');
-        console.log('  Nombre: ' + voice.name);
-        console.log('  Idioma: ' + voice.lang);
-        console.log('  URI de la voz: ' + voice.voiceURI);
-        console.log('  Voz por defecto: ' + voice.default ? 'Sí' : 'No');
-        console.log('  Voz local: ' + voice.localService ? 'Sí' : 'No');
-        console.log('---');
-      });
-        }
-    };
+      analyser.getByteTimeDomainData(dataArray);
 
-    recognition.onerror = function(event) {
-        console.error('Error en el reconocimiento de voz: ' + event.error);
-    };
+      var amplitudeSum = dataArray.reduce(function(a, b) { return a + b; });
+      var averageAmplitude = amplitudeSum / bufferLength;
 
-    function enableRecognition() {
-        startBtn.disabled = false;
+      var waveHeight = averageAmplitude / 128 * 100;
+      waveform.style.height = waveHeight + 'px';
     }
 
-    function speakText(text, callback) {
-        if (text !== '') {
-            const utterance = new SpeechSynthesisUtterance(text);
-            utterance.lang = 'es-ES';
+    animateWaveform();
+  })
+  .catch(function(error) {
+    console.log('Se produjo un error al acceder al micrófono:', error);
+  });
 
-            utterance.onend = function() {
-                if (typeof callback === 'function') {
-                    callback();
-                }
-            };
-
-            utterance.rate = 0.6
-
-            synth.speak(utterance);
-        }
-    }
-
-    startBtn.addEventListener('click', function() {
-        recognition.start();
-        startBtn.disabled = true;
-        stopBtn.disabled = false;
-    });
-
-    stopBtn.addEventListener('click', function() {
-        recognition.stop();
-        startBtn.disabled = false;
-        stopBtn.disabled = true;
-    });
-
-    speakBtn.addEventListener('click', function() {
-        speakText(textInput.value);
-    });
-} else {
-    alert('Tu navegador no soporta la API de reconocimiento de voz o la API de síntesis de voz.');
-}
 
